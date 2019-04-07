c2f2d08f6264a2bc47d09bc9866024f0a85e3f91
nnegi8501j@gmail.com <narender.negi@lexxdigital.com>
2019-04-07 15:51:15 +0530

    Initial

---
+++ database/migrations/2019_04_07_113852_create_merchants_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMerchantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('merchants', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('system_id')->unsigned();
+            $table->foreign('system_id')->references('id')->on('system_type');
+            $table->string('merchant_name');
+            $table->string('merchant_images')->nullable();
+            $table->dateTime('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->dateTime('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('merchants');
+    }
+}
